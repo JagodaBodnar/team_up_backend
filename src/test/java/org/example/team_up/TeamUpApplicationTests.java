c9d55fe70cc537f08973c14cd4622b0d7175f5ef
JagodaBodnar <jagodabodnar90@gmail.com>
2024-04-22 12:15:45 +0200

    Test removed

--- src/test/java/org/example/team_up/TeamUpApplicationTests.java
+++ src/test/java/org/example/team_up/TeamUpApplicationTests.java
@@ -3,11 +3,8 @@ package org.example.team_up;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TeamUpApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
